567e113d26c4bf4593566974f0e6781b39470a75
黄凯 <huangkai@tuya.com>
2018-04-11 10:57:22 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 cd ~/
-git clone https://github.com/L-Zephyr/Drafter.git
+git clone https://github.com/huangdaxiaEX/Drafter.git
 cd Drafter
 swift build -c release -Xswiftc -static-stdlib
 cp -f -r ./Template/ ~/.drafter
